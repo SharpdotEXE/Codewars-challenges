d0168e28b9f95bfe7315a51d8196eb5d597d7acc
SharpdotEXE <69698890+SharpdotEXE@users.noreply.github.com>
2021-01-22 09:02:02 -0600

    Update vowel_count.py

--- vowel_count.py
+++ vowel_count.py
@@ -11,3 +11,13 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 The input string will only consist of lower case letters and/or spaces.
 """
+
+
+def get_count(input_str):
+    num_vowels = 0
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for letter in input_str:
+        if letter in vowels:
+            num_vowels += 1
+
+    return num_vowels
