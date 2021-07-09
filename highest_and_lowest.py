dad743f8eabf7b385befdbafdd793f1105ca2a14
SharpdotEXE <69698890+SharpdotEXE@users.noreply.github.com>
2021-07-09 18:21:25 -0500

    Update highest_and_lowest.py

--- highest_and_lowest.py
+++ highest_and_lowest.py
@@ -5,7 +5,7 @@ creator - Deantwo
 (completed)
 
 
-In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+In this assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
 Example:
 
@@ -18,12 +18,6 @@ All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.
 """
-
-
-### this one was quite satisfying. 
-### I managed to shrink it from about
-### 8 lines to essentially just 1
-### tacky yet fun line
 
 def high_and_low(numbers):
     return f'{sorted([int(i) for i in numbers.split()])[-1]} {sorted([int(i) for i in numbers.split()])[0]}'
